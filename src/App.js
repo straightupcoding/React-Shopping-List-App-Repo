2d31673592ee089ff09eb65fc1eab6dfb2bf6069
greeneterrance92 <58027070+greeneterrance92@users.noreply.github.com>
2020-11-15 00:17:49 -0600

    Initial Commit Shopping List App

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import AddItem from './AddItem';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+function App() {
+  const [items, setItems] = React.useState([]);
+
+  const addItem = (item) => {
+    setItems([item, ...items]);
+  }
+
+  const listItems = items.map((item, index) =>
+    <ListItem key={index}>
+      <ListItemText primary={item.product} secondary={item.amount} />
+    </ListItem>);
+
+  return (
+    <div className="App">
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            SHOPPINGLIST
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <AddItem addItem={addItem} />
+      <List>{listItems}</List>
+    </div>
+  );
+}
+
+export default App;
